118d7c95d106eb04ef269b185fb8832abe4598b6
shin usuda <sasasa.24.blue@gmail.com>
2020-02-14 23:45:11 +0900

    generate address model

--- db/migrate/20200214043245_create_addresses.rb
+++ db/migrate/20200214043245_create_addresses.rb
@@ -1,12 +1,17 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      t.references :user,         foreign_key: true
-      t.references :prefecture,   foreign_key: true
-      t.string     :postal_code,  null: false
-      t.string     :municipality, null: false
-      t.string     :block_number, null: false
+      t.references :user,                  foreign_key: true
+      t.references :prefecture,            foreign_key: true
+      t.string     :ship_family_name,      null: false
+      t.string     :ship_first_name,       null: false
+      t.string     :ship_family_name_kana, null: false
+      t.string     :ship_first_name_kana,  null: false
+      t.string     :postal_code,           null: false
+      t.string     :municipality,          null: false
+      t.string     :block_number,          null: false
       t.string     :apartment_name
+      t.string     :phone_number
       t.timestamps
     end
   end
