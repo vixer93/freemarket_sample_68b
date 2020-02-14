d55580bf39ec2436bbf9e32be9c6cd96ebf0f3d5
shin usuda <sasasa.24.blue@gmail.com>
2020-02-14 12:48:45 +0900

    add action for address registration in devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get "users/address", to: "devise/registrations#new_address"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tests#index"
 end
