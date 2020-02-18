c2ce2230c885ef783b62b2b3ae742c1db1aa4791
KI-RA-CO <kihara4173@icloud.com>
2020-02-18 12:43:20 +0900

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
     get  'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
-  root "tests#index"
   resources :users, only: [:show]
 end
 
