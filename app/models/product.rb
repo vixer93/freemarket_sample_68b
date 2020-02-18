a0c63ee783d2ffc8a12b11f72ff716e6cb65d760
DaikiAoyama <daiki.bce2020@gmail.com>
2020-02-18 21:16:06 +0900

    imageモデルにdestroyを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
   has_one :contract, dependent: delete
   belongs_to :category
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 end
