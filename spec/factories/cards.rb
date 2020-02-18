3450523471d5bfc428fdd7d93193be20a787f238
shin usuda <sasasa.24.blue@gmail.com>
2020-02-18 17:30:13 +0900

    add card model test

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :card do
-    
+    customer_id_token {"aaaaaaaaaaa"}
+    card_id_token     {"fergergergae"}
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Card do
+  describe '#create' do
+    it 'is valid' do
+      user = create(:user)
+      card = build(:card, user_id: user.id)
+      card.valid?
+      expect(card).to be_valid
+    end
+
+    it 'is invalid without customer_id_token' do
+      user = create(:user)
+      card = build(:card, user_id: user.id, customer_id_token: "")
+      card.valid?
+      expect(card.errors[:customer_id_token]).to include("を入力してください")
+    end
+
+    it 'is invalid without card_id_token' do
+      user = create(:user)
+      card = build(:card, user_id: user.id, card_id_token: "")
+      card.valid?
+      expect(card.errors[:card_id_token]).to include("を入力してください")
+    end
+  end
 end
