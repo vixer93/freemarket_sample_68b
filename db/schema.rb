4918ee19d7d7e1817ff5a7120f94b17e5e104ddf
DaikiAoyama <daiki.bce2020@gmail.com>
2020-02-18 15:22:57 +0900

    productテーブルに結びつくimageテーブルの作成とマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_17_064601) do
+ActiveRecord::Schema.define(version: 2020_02_18_054937) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "url", null: false
+    t.integer "products_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
