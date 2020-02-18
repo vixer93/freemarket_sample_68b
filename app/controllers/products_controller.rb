a6eab9be589631e3fefd6e8c205baa7ec1ceb932
DaikiAoyama <daiki.bce2020@gmail.com>
2020-02-18 12:54:52 +0900

    コントローラーにアクションを作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,19 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def create
+  end
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
 end
