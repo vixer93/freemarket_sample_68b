bc16a66d5a2ae9c152af9ac5d2b697cbe0a91178
KI-RA-CO <kihara4173@icloud.com>
2020-02-18 20:41:29 +0900

    fix review application.sass
    modulesファイルに入ってないsassファイルを中に入れて、application.sassで読み込める様に編集した。

--- app/assets/javascripts/new_address.js
+++ app/assets/javascripts/new_address.js
@@ -2,7 +2,6 @@ window.addEventListener("load", function(){
   $(function(){
     $( '#address_ship_family_name' ).on('click',function(){
       let address_ship_family_name = $(this).val();
-      console.log(address_ship_family_name)
       if(address_ship_family_name == 0){
         $('#address_ship_family_name').css('border-color','red')
         $('.add_address_ship_family_name').text('姓を入力してください')
@@ -13,7 +12,6 @@ window.addEventListener("load", function(){
     });
     $( '#address_ship_family_name' ).on('change',function(){
       let address_ship_family_name = $(this).val();
-      console.log(address_ship_family_name)
       if(address_ship_family_name == 0){
         $('#address_ship_family_name').css('border-color','red')
         $('.add_address_ship_family_name').text('姓を入力してください')
@@ -26,7 +24,6 @@ window.addEventListener("load", function(){
 
     $( '#address_ship_first_name' ).on('click',function(){
       let address_ship_first_name = $(this).val();
-      console.log(address_ship_first_name)
       if(address_ship_first_name == 0){
         $('#address_ship_first_name').css('border-color','red')
         $('.add_address_ship_first_name').text('名を入力してください')
@@ -37,7 +34,6 @@ window.addEventListener("load", function(){
     });
     $( '#address_ship_first_name' ).on('change',function(){
       let address_ship_first_name = $(this).val();
-      console.log(address_ship_first_name)
       if(address_ship_first_name == 0){
         $('#address_ship_first_name').css('border-color','red')
         $('.add_address_ship_first_name').text('名を入力してください')
@@ -50,7 +46,6 @@ window.addEventListener("load", function(){
 
     $( '#address_ship_family_name_kana' ).on('click',function(){
       let address_ship_family_name_kana = $(this).val();
-      console.log(address_ship_family_name_kana)
       if(address_ship_family_name_kana == 0){
         $('#address_ship_family_name_kana').css('border-color','red')
         $('.add_address_ship_family_name_kana').text('姓カナを入力してください')
@@ -61,7 +56,6 @@ window.addEventListener("load", function(){
     });
     $( '#address_ship_family_name_kana' ).on('change',function(){
       let address_ship_family_name_kana = $(this).val();
-      console.log(address_ship_family_name_kana)
       if(address_ship_family_name_kana == 0){
         $('#address_ship_family_name_kana').css('border-color','red')
         $('.add_address_ship_family_name_kana').text('姓カナを入力してください')
@@ -74,7 +68,6 @@ window.addEventListener("load", function(){
 
     $( '#address_ship_first_name_kana' ).on('click',function(){
       let address_ship_first_name_kana = $(this).val();
-      console.log(address_ship_first_name_kana)
       if(address_ship_first_name_kana == 0){
         $('#address_ship_first_name_kana').css('border-color','red')
         $('.add_address_ship_first_name_kana').text('名カナを入力してください')
@@ -85,7 +78,6 @@ window.addEventListener("load", function(){
     });
     $( '#address_ship_first_name_kana' ).on('change',function(){
       let address_ship_first_name_kana = $(this).val();
-      console.log(address_ship_first_name_kana)
       if(address_ship_first_name_kana == 0){
         $('#address_ship_first_name_kana').css('border-color','red')
         $('.add_address_ship_first_name_kana').text('名カナを入力してください')
@@ -98,7 +90,6 @@ window.addEventListener("load", function(){
 
     $( '#address_postal_code' ).on('click',function(){
       let address_postal_code = $(this).val();
-      console.log(address_postal_code)
       if(address_postal_code == 0){
         $('#address_postal_code').css('border-color','red')
         $('.add_address_postal_code').text('郵便番号を入力してください')
@@ -109,7 +100,6 @@ window.addEventListener("load", function(){
     });
     $( '#address_postal_code' ).on('change',function(){
       let address_postal_code = $(this).val();
-      console.log(address_postal_code)
       if(address_postal_code == 0){
         $('#address_postal_code').css('border-color','red')
         $('.add_address_postal_code').text('郵便番号を入力してください')
@@ -122,7 +112,6 @@ window.addEventListener("load", function(){
 
     $( '#address_prefecture_id' ).on('click',function(){
       let address_prefecture_id = $(this).val();
-      console.log(address_prefecture_id)
       if(address_prefecture_id == 0){
         $('#address_prefecture_id').css('border-color','red')
         $('.add_address_prefecture_id').text('都道府県を選択してください')
@@ -133,7 +122,6 @@ window.addEventListener("load", function(){
     });
     $( '#address_prefecture_id' ).on('change',function(){
       let address_prefecture_id = $(this).val();
-      console.log(address_prefecture_id)
       if(address_prefecture_id == 0){
         $('#address_prefecture_id').css('border-color','red')
         $('.add_address_prefecture_id').text('都道府県を選択してください')
@@ -146,7 +134,6 @@ window.addEventListener("load", function(){
 
     $( '#address_municipality' ).on('click',function(){
       let address_municipality = $(this).val();
-      console.log(address_municipality)
       if(address_municipality == 0){
         $('#address_municipality').css('border-color','red')
         $('.add_address_municipality').text('市区町村を入力してください')
@@ -157,7 +144,6 @@ window.addEventListener("load", function(){
     });
     $( '#address_municipality' ).on('change',function(){
       let address_municipality = $(this).val();
-      console.log(address_municipality)
       if(address_municipality == 0){
         $('#address_municipality').css('border-color','red')
         $('.add_address_municipality').text('市区町村を入力してください')
@@ -170,7 +156,6 @@ window.addEventListener("load", function(){
 
     $( '#address_block_number' ).on('click',function(){
       let address_block_number = $(this).val();
-      console.log(address_block_number)
       if(address_block_number == 0){
         $('#address_block_number').css('border-color','red')
         $('.add_address_block_number').text('番地を入力してください')
@@ -181,7 +166,6 @@ window.addEventListener("load", function(){
     });
     $( '#address_block_number' ).on('change',function(){
       let address_block_number = $(this).val();
-      console.log(address_block_number)
       if(address_block_number == 0){
         $('#address_block_number').css('border-color','red')
         $('.add_address_block_number').text('番地を入力してください')
