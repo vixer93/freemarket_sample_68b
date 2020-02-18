152a807239b66d6c8e28e04871b8c2d59270c1e4
DaikiAoyama <daiki.bce2020@gmail.com>
2020-02-18 20:25:52 +0900

    [修正]migrationファイルを変更してモデルを修正

--- db/migrate/20200218054937_create_images.rb
+++ db/migrate/20200218054937_create_images.rb
@@ -1,7 +1,7 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-      t.string :url, null: false
+      t.string :name, null: false
       t.integer :products_id, foreign_key: true
       t.timestamps
     end
