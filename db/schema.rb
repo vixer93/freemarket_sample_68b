7b47c60f64c7efdaca175421a14de399459b8408
DaikiAoyama <daiki.bce2020@gmail.com>
2020-02-20 16:19:42 +0900

    カテゴリー機能実装ブランチを merge into したコミット

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2020_02_18_054937) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.integer "product_id"
@@ -45,8 +53,8 @@ ActiveRecord::Schema.define(version: 2020_02_18_054937) do
     t.string "brand"
     t.integer "send_price", default: 0, null: false
     t.integer "ship_day", default: 0, null: false
+    t.integer "buyer_id", null: false
     t.integer "category_id", null: false
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -70,12 +78,4 @@ ActiveRecord::Schema.define(version: 2020_02_18_054937) do
   end
 
   add_foreign_key "addresses", "users"
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
-  end
-
 end
