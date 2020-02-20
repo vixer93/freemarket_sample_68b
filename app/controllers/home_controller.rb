3bd98e936fb6a2fee12c97d8bc274667c427df02
kissy0117 <yk.supercell@gmail.com>
2020-02-20 15:35:15 +0900

    トップページ/多階層/haml完成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @category_parents = Category.where(ancestry: nil).all.order("id ASC")
-    @category_children = Category.where(ancestry: "1").all.order("id ASC")
-    # @lady.children = Category.find(1)
-    # @lady.children.each do |child|
-    #   puts child.name
-    # end
-
+    @categories = Category.where(ancestry: nil).all.order("id ASC")
   end
 end
