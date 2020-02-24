2e17654067c21bf8fb16734e0b7e13fa1babd6e1
DaikiAoyama <daiki.bce2020@gmail.com>
2020-02-24 19:46:44 +0900

    カテゴリー　htmlで親の表示

--- app/assets/javascripts/new_product.js
+++ app/assets/javascripts/new_product.js
@@ -171,6 +171,15 @@ window.addEventListener("load", function(){
       }
     });
 
-
+    $('#parent').on('change', function(){
+      $('#child').html('');  
+      var option;
+      option = '<option value="製品1">製品1</option>';
+      $('#child').append(option);
+      option = '<option value="製品2">製品2</option>';
+      $('#child').append(option);
+      option = '<option value="製品3">製品3</option>';
+      $('#child').append(option);
+    });
   });
 });
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @categories = Category.where(ancestry: nil).all.order("id ASC")
+    
   end
 end
