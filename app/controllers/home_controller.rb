011c0e192118578efee275fa191d0f4c82564187
KI-RA-CO <kihara4173@icloud.com>
2020-02-24 20:03:41 +0900

    created_purchase_confirmation_page
    商品購入確認ページのビューを作成した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,13 @@ class HomeController < ApplicationController
   def index
     @categories = Category.where(ancestry: nil).all.order("id ASC")
   end
+
+  def new
+    
+  end
+
+  # private
+  # def user_params
+  #   params.require(:users)
+  # end
 end
